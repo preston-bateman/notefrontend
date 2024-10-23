77c716a9b7d479dc3d967bd164fce55a0707b3f2
Preston Bateman <prestonbbateman@gmail.com>
2024-10-23 15:26:45 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  //State
+  const[notes, setNotes] = useState(null);
+  const [createForm, setCreateForm] = useState({
+    title: '',
+    body: ''
+  })
+  
+  // Use Effect
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+
+  // Functions
+  const fetchNotes = async () => {
+    const res = await axios.get('http://localhost:3000/notes')
+
+    setNotes(res.data.notes)
+
+    console.log(res)
+  }
+
+  const updateCreateFormField = (e) => {
+    const {name, value} = e.target
+
+    setCreateForm({
+      ...createForm,
+      [name]: value,
+    })
+  }
+
+    return (
+      <div className="App">
+        <div>
+          <h2>Notes:</h2>
+          {notes && notes.map(note => {
+            return (<div key={note._id}>
+              <h3>{note.title}</h3>
+            </div>)
+          })}
+        </div>
+        <div>
+          <h2>Create Note</h2>
+          <form>
+            <input onChange={updateCreateFormField} value={createForm.title} name='title' />
+            <textarea onChange={updateCreateFormField} value={createForm.body} name='body' />
+            <button type="submit">Add Note</button>
+          </form>
+        </div>
+      </div>
   );
 }
 
